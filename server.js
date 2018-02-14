db4b5080213810ae9e6b36fe17369b4e9ec54b96
Anastasia Lucas <anastasialucas@Anastasias-MacBook-Pro.local>
2018-02-14 13:54:56 -0500

    fast csv

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+const zipcodes = require('zipcodes');
+var dist = zipcodes.distance(62959, 90210); 
+// dist = 1662
+
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
