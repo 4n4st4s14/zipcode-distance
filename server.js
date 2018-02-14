b6ce2aca7c25ab9dcaf702734d03f14737bcce7a
Cristopher Ayala Paz <crisayala16@gmail.com>
2018-02-14 14:11:17 -0500

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const csv = require('fast-csv');
 const app = express();
+
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
@@ -11,6 +13,10 @@ app.use(express.static('./public'));
 
 app.post('/getDist', (req, res)=>{
     const {csvString} = req.body;
+    csv.fromString(csvString, {headers: true})
+    .on('data', (data)=>{
+        res.send({parsed: data});
+    });
 });
 
 app.listen(PORT, ()=>{
