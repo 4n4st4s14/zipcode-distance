7b86bc4e6c92035764c4d94369018ecb63699c28
Cristopher Ayala Paz <crisayala16@gmail.com>
2018-02-14 15:14:56 -0500

    update

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const csv = require('fast-csv');
+const distance = require('google-distance');
+const zipcode = require('zipcode');
 const app = express();
-
-const zipcodes = require('zipcodes');
-var dist = zipcodes.distance(62959, 90210);
-
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -20,7 +18,22 @@ app.post('/getDist', (req, res)=>{
     let complete = [];
     csv.fromString(csvString, {headers: true})
     .on('data', (data)=>{
-        complete.push(data);    
+        let location = zipcode.lookup(`${data.zipcode}`);
+        distance.get({
+            origin: location.join(' '),
+            destination: 'Arlington, VA',
+            units: 'imperial',
+            avoid: 'tolls',
+            mode: 'driving'
+        }, (err, distData)=>{
+            if(err){
+                res.status(400).json({error: err});
+            }
+            else{
+                data.distance = distData.distance;
+                complete.push(data);
+            }
+        });
     })
     .on('end', ()=>{
         res.send(complete);
