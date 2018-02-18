ffddb493efa428975d209ce2f7236a4bcb1ccaa4
Cristopher Ayala Paz <crisayala16@gmail.com>
2018-02-18 03:59:36 -0500

    need to test after quota is refilled

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -4,11 +4,23 @@ $(document).ready(function(){
         var reader = new FileReader();
         reader.onload = function(){
             const csvString = reader.result;
+            $('#spinner').html(`<div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-green">
+                    <div class="circle-clipper left">
+                    <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                    </div>
+                    </div>
+                </div>`);
             $.post('/getDist', {
                 csvString: csvString
             }).done((data)=>{
                 if(data){
-                    $('#download').attr('disabled', false);                                    
+                    $('#download').attr('disabled', false);    
+                    $('#spinner').html('');                                
                 }
                 console.log(data);
                 $(document).on('click', '#download', function(){
--- server.js
+++ server.js
@@ -22,68 +22,165 @@ app.post('/getDist', (req, res)=>{
     const {csvString} = req.body;
     console.log('=============');
     let dataArr = csvString.split('\r\n');
+    // console.log('dataArr: ', dataArr);
     let lastIndex = dataArr.length - 1;
-    dataArr.splice(lastIndex, 1);
+    if(!dataArr[lastIndex]){
+        dataArr.splice(lastIndex, 1);
+    }
     dataArr.splice(0, 1);
+    // console.log('dataArr: ', dataArr);
+    // console.log('dataArrLength : ', dataArr.length);
+
+    let apiRuns = Math.ceil(dataArr.length / 10);
     let curIndex = 0;
     let complete = [];
+    let origins = [];
+    let destinations = ['Washington DC', 'Arlington, VA', 'Ashburn, VA'];    
     let csvStream = csv.format({headers: true});
     let writableStream = fs.createWriteStream("my.csv");
+    let successWrites = 0;
     writableStream.on('finish', ()=>{
-        console.log('Done');
+        // console.log('Done');
     });
     csvStream.pipe(writableStream);
     csv.fromString(csvString, {headers: true})
     .on('data', (data)=>{
-        let studentLocation = zipcode.lookup(data.ZIPCODE);
-        console.log(data.Location);
-        let classLocation = data.Location === 'Arlington' ? 'Arlington, VA' : 'Washington DC';
-        let origin = !studentLocation ? 'location not found' : studentLocation.join(' ');
-        let origins = [`${data.ZIPCODE}`];
-        let destinations = [classLocation, 'Ashburn, VA'];
-        distance.matrix(origins, destinations, (err, distData)=>{
-            if(err){
-                console.log('ERROR');
-                console.log(err);
-                res.status(400).end(err);
-            }
-            else{
-                console.log('DIST DATA', curIndex);
-                console.log(distData);                
-                console.log('data: ', data);
-                // console.log(distData.destination_addresses[1]);                  
-                // console.log(distData.rows[0].elements[1]);
-                !studentLocation ? data.State = 'location not found' : data.State = studentLocation[1];
-                data['Distance from Address in Column b'] = distData.status !== 'OK' ? distData.error_message : distData.rows[0].elements[0].distance.text;
-                data['Distance from 45085 University Dr, Ashburn, VA 20147'] = distData.status !== 'OK' ? distData.error_message : distData.rows[0].elements[1].distance.text;    
-                csvStream.write(data);
-                complete.push(data);
-                if(curIndex == dataArr.length -1){
-                    console.log('done');
-                    csvStream.end();   
-                    res.download('./my.csv', (err)=>{
-                        if(err){
-                            console.log(err);
-                            res.status(400).send(err);
+        console.log(dataArr.length);
+        console.log(curIndex);
+        origins.push(`${data.ZIPCODE}`);
+        complete.push(data);
+        if(curIndex === dataArr.length -1){
+            console.log('here');
+            for(let x = 0; x < apiRuns; x++){
+                let begin = x === 0 ? x : x * 10;
+                let end = (x + 1) * 10;
+                let curOrigins = origins.slice(begin, end);
+        
+                distance.matrix(curOrigins, destinations, (err, distances)=>{
+                    if(err){
+                        // console.log('ERROR');
+                        // console.log(err);
+                        res.status(400).end(err);
+                    }
+                    else{
+                        // console.log('DIST DATA', curIndex);
+                        // console.log(distances);                
+                        // console.log('data: ', data);
+                        if (distances.status == 'OK') {
+                            //Every origin
+                            for (let i=0; i < curOrigins.length; i++) {
+                        
+                                // Every Destination for that origin
+                                for (let j = 0; j < destinations.length; j++) {
+    
+                                    let origin = distances.origin_addresses[i];
+                                    let destination = distances.destination_addresses[j];
+    
+                                    // If data is ok
+                                    if (distances.rows[i].elements[j].status === 'OK') {
+                                        // Current Distance 
+                                        let distance = distances.rows[i].elements[j].distance.text;
+    
+                                        //If destinatoin is ashburn fill in ashburn field
+                                        if(destination.includes('Ashburn')){
+                                            complete[begin + i]['Distance from 45085 University Dr, Ashburn, VA 20147'] = distance;    
+                                            successWrites++;
+                                        }
+                                        //If destinatoin DC ashburn fill in DC field                                    
+                                        if(destination.includes('Washington') && complete[i].Location.includes('Washington')){
+                                            complete[begin + i]['Distance from Address in Column b'] = distance;
+                                            complete[begin + i].State = 'DC';
+                                                                                    
+                                        }
+                                        //If destinatoin is arlington fill in arlington field                                    
+                                        if(destination.includes('Arlington') && complete[i].Location.includes('Arlington')){
+                                            complete[begin + i]['Distance from Address in Column b'] = distance;
+                                            complete[begin + i].State = 'VA';
+                                        }
+                                        //When the destinations loop is on its last item
+                                        if(j === destinations.length -1){
+                                            csvStream.write(complete[begin + i]);    
+                                                //When the Origins loop finishes
+                                            if((begin + i) === (origins.length -1)){
+                                                console.log('everything done');
+                                                setTimeout(()=>{
+                                                    csvStream.end();                                                           
+                                                    res.download('./my.csv', (err)=>{
+                                                        if(err){
+                                                            console.log(err);
+                                                            res.status(400).send(err);
+                                                        }
+                                                        else{
+                                                            // res.status(200).json({complete});
+                                                            console.log('end');
+                                                        }
+                                                    });
+                                                }, 9000);
+                                            }                        
+                                        }
+                                    }
+                                    //If Data is not ok 
+                                    else {
+                                        console.log(destination + ' is not reachable by land from ' + origin);
+                                        complete[i].State = 'API Error';
+                                    }
+                                }
+                            }
                         }
                         else{
-                            // res.status(200).json({complete});
-                            console.log('end');
+                            console.log('begin: ', begin);
+                            console.log('DistData: ', distances);
                         }
-                    });
-                    console.log('end2');
-                }
-                else{
-                    console.log('increment');
-                    console.log(curIndex);
-                    console.log(dataArr.length);
-                    curIndex++;
-                }
+                    }
+                });
             }
-        });
+        }
+        else{
+            curIndex++;
+        }
+        
     })
     .on('end', ()=>{
-        
+        // distance.matrix(origins, destinations, (err, distances)=>{
+        //     if(err){
+        //         console.log('ERROR');
+        //         console.log(err);
+        //         res.status(400).end(err);
+        //     }
+        //     else{
+        //         console.log('DIST DATA', curIndex);
+        //         console.log(distances);                
+        //         console.log('data: ', data);
+        //         // console.log(distances.destination_addresses[1]);                  
+        //         // console.log(distances.rows[0].elements[1]);
+        //         !studentLocation ? data.State = 'location not found' : data.State = studentLocation[1];
+        //         data['Distance from Address in Column b'] = distances.status !== 'OK' ? distances.error_message : distances.rows[0].elements[0].distance.text;
+        //         data['Distance from 45085 University Dr, Ashburn, VA 20147'] = distances.status !== 'OK' ? distances.error_message : distances.rows[0].elements[1].distance.text;    
+        //         csvStream.write(data);
+        //         complete.push(data);
+        //         if(curIndex == dataArr.length -1){
+        //             console.log('done');
+        //             csvStream.end();   
+        //             res.download('./my.csv', (err)=>{
+        //                 if(err){
+        //                     console.log(err);
+        //                     res.status(400).send(err);
+        //                 }
+        //                 else{
+        //                     // res.status(200).json({complete});
+        //                     console.log('end');
+        //                 }
+        //             });
+        //             console.log('end2');
+        //         }
+        //         else{
+        //             console.log('increment');
+        //             console.log(curIndex);
+        //             console.log(dataArr.length);
+        //             curIndex++;
+        //         }
+        //     }
+        // });
     });
 });
 
